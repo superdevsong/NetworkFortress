e02a1d1fa9579c63a978af575b9b4e875901b5d3
superdevsong <superdevsong@gamil.com>
2021-11-16 23:48:32 +0900

    code refactoring

--- fortress/src/fortress/ui/Bullet.java
+++ fortress/src/fortress/ui/Bullet.java
@@ -3,16 +3,24 @@ package fortress.ui;
 import java.awt.Image;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.Vector;
 
 import javax.swing.ImageIcon;
 
 public class Bullet {
 	Image image_bullet = new ImageIcon("src/fortress/ui/bullet.png").getImage();
+	private int bullet_width ;
+	private int bullet_height;
 	private double power =0;
 	private double veloX = 1.0;
 	private double veloY = 1.0;
+	private double radian;
 	private int bullet_x;
 	private int bullet_y;
+	public Bullet() {
+		bullet_width = image_bullet.getWidth(null);
+		bullet_height = image_bullet.getHeight(null);
+	}
 	long getTime() {
 		return Timestamp.valueOf(LocalDateTime.now()).getTime();
 	}
@@ -39,12 +47,10 @@ public class Bullet {
 	public Image getImage_bullet() {
 		return image_bullet;
 	}
-	public double getRadian() {
-		return Math.atan(veloY/veloX);
+	public void setRadian() {
+		radian = Math.atan(veloY/veloX);
 	}
-	public Bullet() {
-		
-	}
+	
 	public double getPower() {
 		return power;
 	}
@@ -62,6 +68,121 @@ public class Bullet {
 	}
 	public void setVeloY(double veloY) {
 		this.veloY = veloY;
+	}
+	public void shotBullet(Player now_player,Vector<Player> playerList,int field) {
+		
+				shot=true;
+				int removeIndex = 0;
+				int player_x = now_player.getPlayer_x();
+				int player_y = now_player.getPlayer_y();
+				bullet_x = player_x;//bullet이 나올곳을 플레이어의 위치로해둠
+				bullet_y = player_y;
+				checkHit(now_player,playerList);//플레이어가 총에 맞는지 확인
+				int jump_y;
+				int set;
+				int jump_x;
+				
+				if (!now_player.isDirection()) {// 왼쪽일때
+					System.out.println();
+					veloX=-1.0;
+					setRadian();
+					jump_y = (int) (power * Math.sin(Math.PI + radian));//y 속도 
+					set = (int) (power * Math.sin(Math.PI + radian));//기본 속도
+					jump_x = (int) (power * Math.cos(Math.PI +radian));//x속도
+				} else {// 오른쪽일때
+					veloX = 1.0;
+					setRadian();
+					jump_y = (int) (power * Math.sin(radian));
+					set = (int) (power * Math.sin(radian));
+					jump_x = (int) (power * Math.cos(radian));
+				}
+				System.out.println("y: "+jump_y+" x:"+jump_x);
+				
+				long t1 = getTime();
+				long t2;
+
+				/*
+				 * System.out.println((bullet.getPower() * Math.sin(Math.PI +
+				 * bullet.getRadian())) + " " + (bullet.getPower() * Math.cos(Math.PI +
+				 * bullet.getRadian())));
+				 */
+				while (jump_y > 0) {//속도가 중력가속도를 빼면서 +일떄 상승할때
+					t2 = getTime() - t1;
+					jump_y = set - (int) (t2 / 40);//원래속도에서 중력가속도를 빼서 감속시켜준다.
+					bullet_x=bullet_x + jump_x;
+					bullet_y= bullet_y - jump_y;
+					
+					try {
+						Thread.sleep(45);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				
+				while (bullet_y < field - bullet_height) {//속도가 중력가속도를 빼면서 -일떄 즉반대로 낙하할때
+					t2 = getTime() - t1;
+					jump_y = set - (int) (t2 / 40);//원래속도에서 중력가속도를 빼서 이제는 낙하속도가 점점올라감.
+					bullet_x = bullet_x+jump_x;
+					bullet_y = bullet_y-jump_y;
+					
+					
+					try {
+						Thread.sleep(45);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				
+				if (bullet_y > field - bullet_height) {
+					bullet_y = field - bullet_height;
+				}
+				bullet_x = player_x;
+				bullet_y = player_y;
+				power = 0;
+				shot = false;
+				for(Player player :playerList) {
+					if(player.getPlayer_hp()==0)
+						removeIndex=playerList.indexOf(player);
+				}
+				if(playerList.get(removeIndex).getPlayer_hp()==0)
+				playerList.remove(removeIndex);
+				
+			}
+	void checkHit(Player now_player,Vector<Player> playerList) {//bullet에 맞는지 확인
+		new Thread(new Runnable() {
+			
+
+			@Override
+			public void run() {
+				boolean hit=false;
+				while(true) {
+						for(Player player :playerList) {
+							int player_x = player.getPlayer_x();
+							int player_y = player.getPlayer_y();
+						if(bullet_x<player_x+player.getImage_l().getWidth(null)+10&&
+								bullet_x>=player_x-10&&
+										bullet_y>= player_y) {
+							if(player==now_player)
+								continue;
+							player.setPlayer_hp(player.getPlayer_hp()-20);
+								hit = true;
+						}
+						}
+				try {
+					Thread.sleep(5);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				if(!shot||hit) {
+					if(hit)
+						shot=false;
+					break;
+				}
+				}
+				
+			}
+			
+		}).start();
 	}
 	
 }
--- fortress/src/fortress/ui/MyPanel.java
+++ fortress/src/fortress/ui/MyPanel.java
@@ -35,7 +35,7 @@ public class MyPanel extends JPanel {
 	Player player2 = new Player();
 	Player player3 = new Player();
 	int turn = 0;
-	private int field = 300 - image_t.getHeight(null),removeIndex;
+	private int field = 300 - image_t.getHeight(null);
 	private int background_x = 0, field_x = 0;
 	private Bullet bullet = new Bullet();
 	
@@ -69,7 +69,7 @@ public class MyPanel extends JPanel {
 		}
 		
 		playSound("src/music/music.wav",true);
-		new Thread(new Runnable() {
+		Thread nt = new Thread(new Runnable() {
 
 			@Override
 			public void run() {
@@ -83,7 +83,9 @@ public class MyPanel extends JPanel {
 				}
 
 			}
-		}).start();
+		});
+		nt.setDaemon(true);//main종료할때 같이 종료
+		nt.start();
 		addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent e) {
@@ -91,8 +93,8 @@ public class MyPanel extends JPanel {
 				if (e.getKeyCode() == 37) {
 					// background가 이미지크기에 도달할때까지 옆으로 화면이 흐름
 					//player_x -= 2;
-					now_player.movePlayer_left();// 맵의끝에 갔을때
-					if (background_x != 0 && now_player.getPlayer_x()==0) {
+					now_player.movePlayer_left();//왼쪽으로 이동
+					if (background_x != 0 && now_player.getPlayer_x()==0) {// 맵의끝에 갔을때
 						background_x += 2;
 						field_x += 2;
 					}
@@ -128,108 +130,17 @@ public class MyPanel extends JPanel {
 
 				if (e.getKeyCode() == 32) {
 					
-					new Thread(new Runnable() {
-						@Override
-						public void run() {
+					
+						
 							playSound("src/music/shooting.wav",false);
-							bullet.setShot(true);
-							bullet.setBullet_x(now_player.getPlayer_x());
-							bullet.setBullet_y(now_player.getPlayer_y());
-							new Thread(new Runnable() {
-								
-
-								@Override
-								public void run() {
-									boolean hit=false;
-									while(true) {
-											for(Player player :playerList) {
-											
-											if(bullet.getBullet_x()<=player.getPlayer_x()+player.getImage_l().getWidth(null)+10&&
-													bullet.getBullet_x()>=player.getPlayer_x()-10&&
-													bullet.getBullet_y()>=player.getPlayer_y()) {
-												if(player==now_player)
-													continue;
-												player.setPlayer_hp(player.getPlayer_hp()-20);
-												hit = true;
-											}
-											}
-									try {
-										Thread.sleep(10);
-									} catch (InterruptedException e) {
-										e.printStackTrace();
-									}
-									if(!bullet.isShot()||hit)
-										break;
-									}
-									
-								}
-								
-							}).start();
-							int jump_y;
-							int set;
-							int jump_x;
-							if (!now_player.isDirection()) {// 왼쪽일때
-								bullet.setVeloX(-1.0);
-								jump_y = (int) (bullet.getPower() * Math.sin(Math.PI + bullet.getRadian()));
-								set = (int) (bullet.getPower() * Math.sin(Math.PI + bullet.getRadian()));
-								jump_x = (int) (bullet.getPower() * Math.cos(Math.PI + bullet.getRadian()));
-							} else {// 오른쪽일때
-								bullet.setVeloX(1.0);
-								jump_y = (int) (bullet.getPower() * Math.sin(bullet.getRadian()));
-								set = (int) (bullet.getPower() * Math.sin(bullet.getRadian()));
-								jump_x = (int) (bullet.getPower() * Math.cos(bullet.getRadian()));
-							}
-
+							bullet.shotBullet(now_player, playerList, field);
 							
-							long t1 = bullet.getTime();
-							long t2;
-
-							System.out.println((bullet.getPower() * Math.sin(Math.PI + bullet.getRadian())) + " "
-									+ (bullet.getPower() * Math.cos(Math.PI + bullet.getRadian())));
-							while (jump_y > 0) {
-								t2 = bullet.getTime() - t1;
-								jump_y = set - (int) (t2 / 40);
-								bullet.setBullet_x((int) (bullet.getBullet_x() + jump_x));
-								bullet.setBullet_y((int) (bullet.getBullet_y() - jump_y));
-								
-								try {
-									Thread.sleep(45);
-								} catch (InterruptedException e) {
-									e.printStackTrace();
-								}
-							}
-							
-							while (bullet.getBullet_y() < field - bullet.getImage_bullet().getHeight(null)) {
-								t2 = bullet.getTime() - t1;
-								jump_y = set - (int) (t2 / 40);
-								bullet.setBullet_x((int) (bullet.getBullet_x() + jump_x));
-								bullet.setBullet_y((int) (bullet.getBullet_y() - jump_y));
-								
-								try {
-									Thread.sleep(45);
-								} catch (InterruptedException e) {
-									e.printStackTrace();
-								}
-							}
-							
-							if (bullet.getBullet_y() > field - bullet.getImage_bullet().getHeight(null)) {
-								bullet.setBullet_y(field - bullet.getImage_bullet().getHeight(null));
-							}
-							bullet.setBullet_x(now_player.getPlayer_x());
-							bullet.setBullet_y(now_player.getPlayer_y());
-							bullet.setPower(0);
-							bullet.setShot(false);
-							for(Player player :playerList) {
-								if(player.getPlayer_hp()==0)
-									removeIndex=playerList.indexOf(player);
-							}
-							if(playerList.get(removeIndex).getPlayer_hp()==0)
-							playerList.remove(removeIndex);
 							if(turn>=playerList.size())
 								turn=0;
 							now_player = playerList.get(turn++);
-						}
-					}).start();
+							
+					
+					
 				}
 			}
 		});
