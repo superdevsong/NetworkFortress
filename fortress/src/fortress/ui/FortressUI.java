1cc77957b53a784186a8edbcbc5777ed7f647a93
superdevsong <superdevsong@gamil.com>
2021-11-30 15:37:14 +0900

    add simple client function

--- fortress/src/fortress/ui/FortressUI.java
+++ fortress/src/fortress/ui/FortressUI.java
@@ -1,6 +1,9 @@
 package fortress.ui;
 
 import java.awt.EventQueue;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import java.awt.Graphics;
 import java.awt.Image;
 import javax.swing.JFrame;
@@ -13,6 +16,8 @@ import net.miginfocom.swing.MigLayout;
 import test.Main;
 
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.plaf.basic.BasicOptionPaneUI.ButtonActionListener;
 
 import java.awt.event.MouseEvent;
@@ -34,22 +39,20 @@ import java.awt.event.MouseAdapter;
 public class FortressUI {
 	public static final int SCREEN_WIDTH = 960;
 	public static final int SCREEN_HEIGHT = 640;
-
+	JTextField txtIpAddress;
+	JTextField txtPortNumber;
+	JTextField txtUserName;
 	private JFrame frame;
 	int x=100,y=200;
 	int alpha=0;
 	int direction=1;
-	private Image screenImage;
-	private Graphics screenGraphic;
+	
 
 	private Image mainImage = new ImageIcon("src/image/Title.jpg").getImage();
-	private JLabel menuBar = new JLabel(new ImageIcon("src/image/menuBar.png"));
 	
-    private JButton exitButton = new JButton(new ImageIcon("src/image/exitButton.jpg"));
 	private JButton startButton = new JButton(new ImageIcon("src/image/startButton.jpg"));
 	private JButton quitButton = new JButton(new ImageIcon("src/image/quitButton.jpg"));
 	private JPanel intro_panel;
-	private int mouseX, mouseY;
 
 	/**
 	 * Launch the application.
@@ -80,7 +83,7 @@ public class FortressUI {
 	 */
 	private void initialize() {
 		
-		frame = new JFrame() {};
+		frame = new JFrame();
 		frame.setTitle("Battle Fortress");
 		frame.setResizable(false);
 		frame.setBounds(100, 100, SCREEN_WIDTH, SCREEN_HEIGHT);
@@ -154,6 +157,64 @@ public class FortressUI {
 		private void gameStart() {
 			remove(quitButton);
 			remove(startButton);
+			JLabel lblNewLabel = new JLabel("User Name");
+			lblNewLabel.setBounds(12, 39, 82, 33);
+			add(lblNewLabel);
+			
+			txtUserName = new JTextField();
+			txtUserName.setHorizontalAlignment(SwingConstants.CENTER);
+			txtUserName.setBounds(101, 39, 116, 33);
+			add(txtUserName);
+			txtUserName.setColumns(10);
+			
+			JLabel lblIpAddress = new JLabel("IP Address");
+			lblIpAddress.setBounds(12, 100, 82, 33);
+			add(lblIpAddress);
+			
+			txtIpAddress = new JTextField();
+			txtIpAddress.setHorizontalAlignment(SwingConstants.CENTER);
+			txtIpAddress.setText("127.0.0.1");
+			txtIpAddress.setColumns(10);
+			txtIpAddress.setBounds(101, 100, 116, 33);
+			add(txtIpAddress);
+			
+			JLabel lblPortNumber = new JLabel("Port Number");
+			lblPortNumber.setBounds(12, 163, 82, 33);
+			add(lblPortNumber);
+			
+			txtPortNumber = new JTextField();
+			txtPortNumber.setText("30000");
+			txtPortNumber.setHorizontalAlignment(SwingConstants.CENTER);
+			txtPortNumber.setColumns(10);
+			txtPortNumber.setBounds(101, 163, 116, 33);
+			add(txtPortNumber);
+			
+			JButton btnConnect = new JButton("Connect");
+			btnConnect.setBounds(12, 223, 205, 38);
+			add(btnConnect);
+			Myaction action = new Myaction();
+			btnConnect.addActionListener(action);
+			txtUserName.addActionListener(action);
+			txtIpAddress.addActionListener(action);
+			txtPortNumber.addActionListener(action);
+			repaint();
+	
+		}
+		}.intro();
+		
+		frame.getContentPane().add(intro_panel, BorderLayout.CENTER);
+		
+		
+		
+
+	}
+	class Myaction implements ActionListener // 내부클래스로 액션 이벤트 처리 클래스
+	{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String username = txtUserName.getText().trim();
+			String ip_addr = txtIpAddress.getText().trim();
+			String port_no = txtPortNumber.getText().trim();
 			Thread start = new Thread(new Runnable() {
 
 				@Override
@@ -163,7 +224,7 @@ public class FortressUI {
 					while(alpha<254) {
 						alpha +=5;
 						try {
-							repaint();
+							intro_panel.repaint();
 							Thread.sleep(10);
 						} catch (InterruptedException e) {
 							// TODO Auto-generated catch block
@@ -207,15 +268,9 @@ public class FortressUI {
 					
 					}
 					}}).start();
-	
+			JavaObjClientView view = new JavaObjClientView(username, ip_addr, port_no);
+			
 		}
-		}.intro();
-		
-		frame.getContentPane().add(intro_panel, BorderLayout.CENTER);
-		
-		
-		
-
 	}
 
 }
---
+++ fortress/src/fortress/ui/JavaObjClientView.java
@@ -0,0 +1,9 @@
+package fortress.ui;
+
+public class JavaObjClientView {
+
+	public JavaObjClientView(String username, String ip_addr, String port_no) {
+		
+	}
+
+}
